433ae3c1d815c10da01cce0f644ebafc6a73da32
Steven <gonghaima@hotmail.com>
2019-09-02 09:17:42 +1000

    Write resolvers on types

--- start/server/src/resolvers.js
+++ start/server/src/resolvers.js
@@ -24,5 +24,32 @@ module.exports = {
     launch: (_, { id }, { dataSources }) =>
       dataSources.launchAPI.getLaunchById({ launchId: id }),
     me: (_, __, { dataSources }) => dataSources.userAPI.findOrCreateUser()
+  },
+  Mission: {
+    // make sure the default size is 'large' in case user doesn't specify
+    missionPatch: (mission, { size } = { size: "LARGE" }) => {
+      return size === "SMALL"
+        ? mission.missionPatchSmall
+        : mission.missionPatchLarge;
+    }
+  },
+  Launch: {
+    isBooked: async (launch, _, { dataSources }) =>
+      dataSources.userAPI.isBookedOnLaunch({ launchId: launch.id })
+  },
+  User: {
+    trips: async (_, __, { dataSources }) => {
+      // get ids of launches by user
+      const launchIds = await dataSources.userAPI.getLaunchIdsByUser();
+
+      if (!launchIds.length) return [];
+
+      // look up those launches by their ids
+      return (
+        dataSources.launchAPI.getLaunchesByIds({
+          launchIds
+        }) || []
+      );
+    }
   }
 };
