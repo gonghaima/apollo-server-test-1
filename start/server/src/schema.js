f3e175bfb6fe9f5cff3deeceee2ab42bc6a8df13
Steven <gonghaima@hotmail.com>
2019-09-01 16:54:43 +1000

    fix - Error: There can be only one type named

--- start/server/src/schema.js
+++ start/server/src/schema.js
@@ -2,11 +2,47 @@ const { gql } = require("apollo-server");
 
 const typeDefs = gql`
   type Query {
-    launches: [Launch]!
+    launches(
+      """
+      The number of results to show. Must be >= 1. Default = 20
+      """
+      pageSize: Int
+      """
+      If you add a cursor here, it will only return results _after_ this cursor
+      """
+      after: String
+    ): LaunchConnection!
     launch(id: ID!): Launch
-    # Queries for the current user
     me: User
   }
+
+  type Mutation {
+    # if false, signup failed -- check errors
+    bookTrips(launchIds: [ID]!): TripUpdateResponse!
+
+    # if false, cancellation failed -- check errors
+    cancelTrip(launchId: ID!): TripUpdateResponse!
+
+    login(email: String): String # login token
+  }
+
+  type TripUpdateResponse {
+    success: Boolean!
+    message: String
+    launches: [Launch]
+  }
+
+  """
+  Simple wrapper around our list of launches that contains a cursor to the
+  last item in the list. Pass this cursor to the launches query to fetch results
+  after these.
+  """
+  type LaunchConnection {
+    cursor: String!
+    hasMore: Boolean!
+    launches: [Launch]!
+  }
+
   type Launch {
     id: ID!
     site: String
@@ -14,6 +50,7 @@ const typeDefs = gql`
     rocket: Rocket
     isBooked: Boolean!
   }
+
   type Rocket {
     id: ID!
     name: String
@@ -34,27 +71,6 @@ const typeDefs = gql`
   enum PatchSize {
     SMALL
     LARGE
-  }
-  type Mutation {
-    # if false, booking trips failed -- check errors
-    bookTrips(launchIds: [ID]!): TripUpdateResponse!
-
-    # if false, cancellation failed -- check errors
-    cancelTrip(launchId: ID!): TripUpdateResponse!
-
-    login(email: String): String # login token
-  }
-  type TripUpdateResponse {
-    success: Boolean!
-    message: String
-    launches: [Launch]
-  }
-  type Launch {
-    id: ID!
-    site: String
-    mission: Mission
-    rocket: Rocket
-    isBooked: Boolean!
   }
 `;
 
