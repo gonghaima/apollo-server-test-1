2309db432237894e8e2cc7d73c0930d1437216bc
Steven <gonghaima@hotmail.com>
2019-09-02 13:23:07 +1000

    Authenticate users

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -10,8 +10,19 @@ const UserAPI = require("./datasources/user");
 const store = createStore();
 
 const server = new ApolloServer({
-  typeDefs,
+  context: async ({ req }) => {
+    // simple auth check on every request
+    const auth = (req.headers && req.headers.authorization) || "";
+    const email = Buffer.from(auth, "base64").toString("ascii");
+    // if the email isn't formatted validly, return null for user
+    if (!isEmail.validate(email)) return { user: null };
+    // find a user by their email
+    const users = await store.users.findOrCreate({ where: { email } });
+    const user = users && users[0] ? users[0] : null;
 
+    return { user: { ...user.dataValues } };
+  },
+  typeDefs,
   resolvers,
   dataSources: () => ({
     launchAPI: new LaunchAPI(),
