88b14737427707831821bf989afb15af8c2b3829
Steven <gonghaima@hotmail.com>
2019-09-03 11:40:55 +1000

    Make your first query

--- start/client/src/index.js
+++ start/client/src/index.js
@@ -1,3 +1,4 @@
+import gql from "graphql-tag";
 import { ApolloClient } from "apollo-client";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { HttpLink } from "apollo-link-http";
@@ -11,3 +12,18 @@ const client = new ApolloClient({
   cache,
   link
 });
+// ... above is the instantiation of the client object.
+client
+  .query({
+    query: gql`
+      query GetLaunch {
+        launch(id: 56) {
+          id
+          mission {
+            name
+          }
+        }
+      }
+    `
+  })
+  .then(result => console.log(result));
