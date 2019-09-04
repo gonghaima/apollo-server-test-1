09c0006fee5cea9c6a05444079e4e013639e8d59
Steven <gonghaima@hotmail.com>
2019-09-04 11:42:33 +1000

    Fetching a list

--- start/client/src/index.js
+++ start/client/src/index.js
@@ -2,6 +2,10 @@ import gql from "graphql-tag";
 import { ApolloClient } from "apollo-client";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { HttpLink } from "apollo-link-http";
+import { ApolloProvider } from "@apollo/react-hooks";
+import React from "react";
+import ReactDOM from "react-dom";
+import Pages from "./pages";
 
 const cache = new InMemoryCache();
 const link = new HttpLink({
@@ -12,18 +16,10 @@ const client = new ApolloClient({
   cache,
   link
 });
-// ... above is the instantiation of the client object.
-client
-  .query({
-    query: gql`
-      query GetLaunch {
-        launch(id: 56) {
-          id
-          mission {
-            name
-          }
-        }
-      }
-    `
-  })
-  .then(result => console.log(result));
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Pages />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
--- start/client/src/pages/launches.js
+++ start/client/src/pages/launches.js
@@ -1,5 +1,43 @@
-import React from 'react';
+import React, { Fragment } from "react";
+import { useQuery } from "@apollo/react-hooks";
+import gql from "graphql-tag";
+
+import { LaunchTile, Header, Button, Loading } from "../components";
+
+const GET_LAUNCHES = gql`
+  query launchList($after: String) {
+    launches(after: $after) {
+      cursor
+      hasMore
+      launches {
+        id
+        isBooked
+        rocket {
+          id
+          name
+        }
+        mission {
+          name
+          missionPatch
+        }
+      }
+    }
+  }
+`;
 
 export default function Launches() {
-  return <div />;
+  const { data, loading, error } = useQuery(GET_LAUNCHES);
+  if (loading) return <Loading />;
+  if (error) return <p>ERROR</p>;
+
+  return (
+    <Fragment>
+      <Header />
+      {data.launches &&
+        data.launches.launches &&
+        data.launches.launches.map(launch => (
+          <LaunchTile key={launch.id} launch={launch} />
+        ))}
+    </Fragment>
+  );
 }
