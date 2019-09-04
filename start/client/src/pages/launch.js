f4869859858216dd7eb14bf2b76aa53d3395377a
Steven <gonghaima@hotmail.com>
2019-09-05 09:11:28 +1000

    Fetching a single launch

--- start/client/src/pages/launch.js
+++ start/client/src/pages/launch.js
@@ -1,5 +1,42 @@
-import React from 'react';
+import React, { Fragment } from "react";
+import { useQuery } from "@apollo/react-hooks";
+import gql from "graphql-tag";
 
-export default function Launch() {
-  return <div />;
+import { Loading, Header, LaunchDetail } from "../components";
+import { ActionButton } from "../containers";
+
+export const GET_LAUNCH_DETAILS = gql`
+  query LaunchDetails($launchId: ID!) {
+    launch(id: $launchId) {
+      id
+      site
+      isBooked
+      rocket {
+        id
+        name
+        type
+      }
+      mission {
+        name
+        missionPatch
+      }
+    }
+  }
+`;
+export default function Launch({ launchId }) {
+  const { data, loading, error } = useQuery(GET_LAUNCH_DETAILS, {
+    variables: { launchId }
+  });
+  if (loading) return <Loading />;
+  if (error) return <p>ERROR: {error.message}</p>;
+
+  return (
+    <Fragment>
+      <Header image={data.launch.mission.missionPatch}>
+        {data.launch.mission.name}
+      </Header>
+      <LaunchDetail {...data.launch} />
+      <ActionButton {...data.launch} />
+    </Fragment>
+  );
 }
