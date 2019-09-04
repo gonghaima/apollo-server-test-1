2d64a11220ca7ec948eb51991e9f396375780a63
Steven <gonghaima@hotmail.com>
2019-09-05 09:46:00 +1000

    Update data with useMutation

--- start/client/src/pages/login.js
+++ start/client/src/pages/login.js
@@ -1,5 +1,16 @@
-import React from 'react';
+import React from "react";
+import { useApolloClient, useMutation } from "@apollo/react-hooks";
+import gql from "graphql-tag";
+
+import { LoginForm, Loading } from "../components";
+
+const LOGIN_USER = gql`
+  mutation login($email: String!) {
+    login(email: $email)
+  }
+`;
 
 export default function Login() {
-  return <div />;
+  const [login, { data }] = useMutation(LOGIN_USER);
+  return <LoginForm login={login} />;
 }
