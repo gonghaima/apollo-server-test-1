0ea7edc74323c2611ffb599436d65ba73ee6993f
Steven <gonghaima@hotmail.com>
2019-09-04 11:44:28 +1000

    Build a paginated list

--- start/client/src/pages/launches.js
+++ start/client/src/pages/launches.js
@@ -26,18 +26,40 @@ const GET_LAUNCHES = gql`
 `;
 
 export default function Launches() {
-  const { data, loading, error } = useQuery(GET_LAUNCHES);
+  const { data, loading, error, fetchMore } = useQuery(GET_LAUNCHES);
   if (loading) return <Loading />;
   if (error) return <p>ERROR</p>;
 
   return (
     <Fragment>
       <Header />
-      {data.launches &&
-        data.launches.launches &&
-        data.launches.launches.map(launch => (
-          <LaunchTile key={launch.id} launch={launch} />
-        ))}
+      {data.launches && data.launches.hasMore && (
+        <Button
+          onClick={() =>
+            fetchMore({
+              variables: {
+                after: data.launches.cursor
+              },
+
+              updateQuery: (prev, { fetchMoreResult, ...rest }) => {
+                if (!fetchMoreResult) return prev;
+                return {
+                  ...fetchMoreResult,
+                  launches: {
+                    ...fetchMoreResult.launches,
+                    launches: [
+                      ...prev.launches.launches,
+                      ...fetchMoreResult.launches.launches
+                    ]
+                  }
+                };
+              }
+            })
+          }
+        >
+          Load More
+        </Button>
+      )}
     </Fragment>
   );
 }
