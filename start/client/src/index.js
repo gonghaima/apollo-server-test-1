678516712093f52540b5264ada684a90fbfc14bc
Steven <gonghaima@hotmail.com>
2019-09-07 08:43:06 +1000

    Query local data

--- start/client/src/index.js
+++ start/client/src/index.js
@@ -2,11 +2,27 @@ import gql from "graphql-tag";
 import { ApolloClient } from "apollo-client";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { HttpLink } from "apollo-link-http";
-import { ApolloProvider } from "@apollo/react-hooks";
+import { ApolloProvider, useQuery } from "@apollo/react-hooks";
 import { resolvers, typeDefs } from "./resolvers";
 import React from "react";
 import ReactDOM from "react-dom";
 import Pages from "./pages";
+
+import Login from "./pages/login";
+import injectStyles from "./styles";
+
+const IS_LOGGED_IN = gql`
+  query IsUserLoggedIn {
+    isLoggedIn @client
+  }
+`;
+
+function IsLoggedIn() {
+  const { data } = useQuery(IS_LOGGED_IN);
+  return data.isLoggedIn ? <Pages /> : <Login />;
+}
+
+injectStyles();
 
 const cache = new InMemoryCache();
 
