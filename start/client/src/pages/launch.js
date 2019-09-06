694e6888a693a28683d7b3a8ea5cab7946835801
Steven <gonghaima@hotmail.com>
2019-09-07 09:36:08 +1000

    Adding virtual fields to server data

--- start/client/src/pages/launch.js
+++ start/client/src/pages/launch.js
@@ -10,6 +10,7 @@ export const GET_LAUNCH_DETAILS = gql`
   query LaunchDetails($launchId: ID!) {
     launch(id: $launchId) {
       id
+      isInCart @client
       site
       isBooked
       rocket {
--- start/client/src/resolvers.js
+++ start/client/src/resolvers.js
@@ -1,11 +1,13 @@
 import gql from "graphql-tag";
 
+/******************************************************************************************************************************************************
+ * To add a virtual field, first extend the type of the data you're adding the field to in your client schema. Here, we're extending the Launch type: *
+ ******************************************************************************************************************************************************/
 export const typeDefs = gql`
   extend type Query {
     isLoggedIn: Boolean!
     cartItems: [ID!]!
   }
-
   extend type Launch {
     isInCart: Boolean!
   }
@@ -15,4 +17,14 @@ export const typeDefs = gql`
   }
 `;
 
-export const resolvers = {};
+/********************************************************************************************************
+ * specify a client resolver on the Launch type to tell Apollo Client how to resolve your virtual field *
+ ********************************************************************************************************/
+export const resolvers = {
+  Launch: {
+    isInCart: (launch, _, { cache }) => {
+      const { cartItems } = cache.readQuery({ query: GET_CART_ITEMS });
+      return cartItems.includes(launch.id);
+    }
+  }
+};
