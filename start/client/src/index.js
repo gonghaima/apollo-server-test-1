ee321622b7ed2e14ce2609aeb16232bb27367810
Steven <gonghaima@hotmail.com>
2019-09-06 11:30:09 +1000

    Attach authorization headers to the request

--- start/client/src/index.js
+++ start/client/src/index.js
@@ -8,13 +8,22 @@ import ReactDOM from "react-dom";
 import Pages from "./pages";
 
 const cache = new InMemoryCache();
-const link = new HttpLink({
-  uri: "http://localhost:4000/"
-});
 
 const client = new ApolloClient({
   cache,
-  link
+  link: new HttpLink({
+    uri: "http://localhost:4000/graphql",
+    headers: {
+      authorization: localStorage.getItem("token")
+    }
+  })
+});
+
+cache.writeData({
+  data: {
+    isLoggedIn: !!localStorage.getItem("token"),
+    cartItems: []
+  }
 });
 
 ReactDOM.render(
