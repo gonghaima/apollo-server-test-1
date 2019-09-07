8258a5ab447324954aa00833bb403ec4f80fd097
Steven <gonghaima@hotmail.com>
2019-09-07 10:13:09 +1000

    Local resolvers

--- start/client/src/resolvers.js
+++ start/client/src/resolvers.js
@@ -26,5 +26,17 @@ export const resolvers = {
       const { cartItems } = cache.readQuery({ query: GET_CART_ITEMS });
       return cartItems.includes(launch.id);
     }
+  },
+  Mutation: {
+    addOrRemoveFromCart: (_, { id }, { cache }) => {
+      const { cartItems } = cache.readQuery({ query: GET_CART_ITEMS });
+      const data = {
+        cartItems: cartItems.includes(id)
+          ? cartItems.filter(i => i !== id)
+          : [...cartItems, id]
+      };
+      cache.writeQuery({ query: GET_CART_ITEMS, data });
+      return data.cartItems;
+    }
   }
 };
