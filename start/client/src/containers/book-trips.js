8acb8950e07d7e8efdbf543c491b6d14235b23d3
Steven <gonghaima@hotmail.com>
2019-09-09 09:08:45 +1000

    fix warning - on book-trips container

--- start/client/src/containers/book-trips.js
+++ start/client/src/containers/book-trips.js
@@ -5,7 +5,8 @@ import gql from "graphql-tag";
 import Button from "../components/button";
 import { GET_LAUNCH } from "./cart-item";
 
-const BOOK_TRIPS = gql`
+export { GET_LAUNCH };
+export const BOOK_TRIPS = gql`
   mutation BookTrips($launchIds: [ID]!) {
     bookTrips(launchIds: $launchIds) {
       success
@@ -17,21 +18,21 @@ const BOOK_TRIPS = gql`
     }
   }
 `;
-
 /***********************
  * Direct cache writes *
  ***********************/
 export default function BookTrips({ cartItems }) {
-  const [bookTrips, { data, loading, error }] = useMutation(BOOK_TRIPS, {
+  const [bookTrips, { data }] = useMutation(BOOK_TRIPS, {
+    variables: { launchIds: cartItems },
     refetchQueries: cartItems.map(launchId => ({
       query: GET_LAUNCH,
       variables: { launchId }
     })),
-
     update(cache) {
       cache.writeData({ data: { cartItems: [] } });
     }
   });
+
   return data && data.bookTrips && !data.bookTrips.success ? (
     <p data-testid="message">{data.bookTrips.message}</p>
   ) : (
