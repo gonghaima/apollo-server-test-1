46947fed02b0360f009dcd0623ad459f49af86f0
Steven <gonghaima@hotmail.com>
2019-09-10 14:46:41 +1000

    use data from mocked file

--- start/server/src/datasources/product.js
+++ start/server/src/datasources/product.js
@@ -1,5 +1,5 @@
 const { RESTDataSource } = require("apollo-datasource-rest");
-const { data } = require("./mock/productsData");
+const data = require("./mock/productsData");
 
 class ProductAPI extends RESTDataSource {
   constructor() {
@@ -9,23 +9,8 @@ class ProductAPI extends RESTDataSource {
   async getAllProducts() {
     // const response = await Promise.resolve(data);
     // return Array.isArray(response) ? response : [];
-    // return data;
-    return [
-      {
-        id: 1,
-        price: "$87.68",
-        product_name: "Amitriptyline Hydrochloride",
-        description: "synergize efficient metrics",
-        product_image: "http://dummyimage.com/307x328.bmp/ff4444/ffffff"
-      },
-      {
-        id: 2,
-        price: "$58.53",
-        product_name: "Zicam",
-        description: "repurpose world-class metrics",
-        product_image: "http://dummyimage.com/345x342.jpg/dddddd/000000"
-      }
-    ];
+    console.log(data);
+    return data;
   }
 }
 
