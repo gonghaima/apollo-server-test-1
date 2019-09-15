d711e8b2710b407550ab67443e32190c12581d2f
Steven <gonghaima@hotmail.com>
2019-09-15 18:59:00 +1000

    add seed.js to pupolate data into sqlite

---
+++ start/server/src/datasources/mock/productsData-one.js
@@ -0,0 +1,10 @@
+const data = [
+  {
+    id: 1,
+    price: "$87.68",
+    product_name: "Amitriptyline Hydrochloride",
+    description: "synergize efficient metrics",
+    product_image: "http://dummyimage.com/307x328.bmp/ff4444/ffffff"
+  }
+];
+module.exports = data;
---
+++ start/server/src/datasources/mock/seed.js
@@ -0,0 +1,65 @@
+const SQL = require("sequelize");
+const data = require("./productsData");
+
+function createStore() {
+  debugger;
+  const Op = SQL.Op;
+  const operatorsAliases = {
+    $in: Op.in
+  };
+
+  const db = new SQL("database", "username", "password", {
+    dialect: "sqlite",
+    storage: "../../../store.sqlite",
+    operatorsAliases,
+    logging: false
+  });
+
+  const products = db.define("products", {
+    id: {
+      type: SQL.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    price: SQL.INTEGER,
+    productName: SQL.STRING,
+    productImage: SQL.STRING,
+    description: SQL.STRING,
+    createdAt: SQL.DATE,
+    updatedAt: SQL.DATE
+  });
+
+  return { products };
+}
+
+const store = createStore();
+
+module.exports.init = function populateData() {
+  debugger;
+  try {
+    data.map(({ price, product_name, product_image, description }) => {
+      const pc = +price.slice(1) * 100;
+      const nm = product_name.slice();
+      const im = product_image.slice();
+      store.products
+        .create({
+          price: pc,
+          productName: product_name,
+          productImage: product_image,
+          description: description,
+          createAt: new Date(),
+          updatedAt: new Date()
+        })
+        .then(function(user) {
+          // you can now access the newly created user
+          console.log("success", user.toJSON());
+        })
+        .catch(function(err) {
+          // print the error details
+          console.log(err);
+        });
+    });
+  } catch (error) {
+    throw error;
+  }
+};
