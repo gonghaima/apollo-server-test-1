7c7f93430e245341ceb0f5e74363cda0a30d18b6
Steven <gonghaima@hotmail.com>
2019-09-17 10:07:05 +1000

    start to work on product page

--- start/client/src/pages/products.js
+++ start/client/src/pages/products.js
@@ -1,5 +1,20 @@
 import React from "react";
+import { useQuery } from "@apollo/react-hooks";
+import gql from "graphql-tag";
+import { LaunchTile, Header, Button, Loading } from "../components";
 
-export default function Launches() {
+export const GET_PRODUCTS = gql`
+  query GetProducts($after: String) {
+    products {
+      price
+      productName
+      productImage
+      description
+    }
+  }
+`;
+
+export default function Products() {
+  const { data, loading, error, fetchMore } = useQuery(GET_PRODUCTS);
   return <h1>Hello world</h1>;
 }
