e0a43798aa8c14fa95535eea53813064492e22d5
Steven <gonghaima@hotmail.com>
2019-09-17 10:15:48 +1000

    product price displays now

--- start/client/src/pages/products.js
+++ start/client/src/pages/products.js
@@ -1,10 +1,10 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { useQuery } from "@apollo/react-hooks";
 import gql from "graphql-tag";
 import { LaunchTile, Header, Button, Loading } from "../components";
 
 export const GET_PRODUCTS = gql`
-  query GetProducts($after: String) {
+  query GetProducts {
     products {
       price
       productName
@@ -16,5 +16,18 @@ export const GET_PRODUCTS = gql`
 
 export default function Products() {
   const { data, loading, error, fetchMore } = useQuery(GET_PRODUCTS);
-  return <h1>Hello world</h1>;
+  if (loading) {
+    return <Loading />;
+  }
+  if (error) {
+    debugger;
+    console.log(error);
+    return <p>ERROR</p>;
+  }
+  return (
+    <Fragment>
+      <Header />
+      {data.products && data.products.map(product => <p>{product.price}</p>)}
+    </Fragment>
+  );
 }
