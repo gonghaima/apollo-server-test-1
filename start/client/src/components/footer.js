9a386a1f76f429248d9462823868675c62822362
Steven <gonghaima@hotmail.com>
2019-09-23 11:54:57 +1000

    add a product icon svg

--- start/client/src/components/footer.js
+++ start/client/src/components/footer.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import styled from 'react-emotion';
+import React from "react";
+import styled from "react-emotion";
 
-import MenuItem from './menu-item';
-import LogoutButton from '../containers/logout-button';
-import { ReactComponent as HomeIcon } from '../assets/icons/home.svg';
-import { ReactComponent as CartIcon } from '../assets/icons/cart.svg';
-import { ReactComponent as ProfileIcon } from '../assets/icons/profile.svg';
-import { colors, unit } from '../styles';
+import MenuItem from "./menu-item";
+import LogoutButton from "../containers/logout-button";
+import { ReactComponent as HomeIcon } from "../assets/icons/home.svg";
+import { ReactComponent as CartIcon } from "../assets/icons/cart.svg";
+import { ReactComponent as ProfileIcon } from "../assets/icons/profile.svg";
+import { ReactComponent as ProductIcon } from "../assets/icons/product.svg";
+import { colors, unit } from "../styles";
 
 export default function Footer() {
   return (
@@ -21,7 +22,7 @@ export default function Footer() {
           Cart
         </MenuItem>
         <MenuItem to="/profile">
-          <ProfileIcon />
+          <ProductIcon />
           Profile
         </MenuItem>
         <LogoutButton />
@@ -34,19 +35,19 @@ export default function Footer() {
  * STYLED COMPONENTS USED IN THIS FILE ARE BELOW HERE
  */
 
-const Container = styled('footer')({
+const Container = styled("footer")({
   flexShrink: 0,
-  marginTop: 'auto',
-  backgroundColor: 'white',
+  marginTop: "auto",
+  backgroundColor: "white",
   color: colors.textSecondary,
-  position: 'sticky',
-  bottom: 0,
+  position: "sticky",
+  bottom: 0
 });
 
-const InnerContainer = styled('div')({
-  display: 'flex',
-  alignItems: 'center',
+const InnerContainer = styled("div")({
+  display: "flex",
+  alignItems: "center",
   maxWidth: 460,
   padding: unit * 2.5,
-  margin: '0 auto',
+  margin: "0 auto"
 });
