1608454879548109f7e06e483cd718e7b29c2d89
Steven <gonghaima@hotmail.com>
2019-10-25 07:36:41 +1100

    validating dollar value, must contains $ for input value

--- start/client/src/components/form/validation.js
+++ start/client/src/components/form/validation.js
@@ -10,6 +10,7 @@ export const vRule = Yup.object().shape({
     productName: Yup.string()
         .required('Required'),
     price: Yup.string()
+        .matches(/\$+/, { message: "input must contain $", excludeEmptyString: true })
         .required('Required'),
     description: Yup.string()
         .required('Required')
